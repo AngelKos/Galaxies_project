8788254ba3d33dc65c74de728320caf9b1e18fec
SergeRepin <43678269+SergeRepin@users.noreply.github.com>
2018-09-28 08:31:47 -0700

    Add files via upload

---
+++ Galaxies_console/Galaxies_console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxies_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Cluster cluster = new Cluster(s);
+            cluster.Finding_clusters();
+            Console.WriteLine("Analysis of file is ended.");
+            Console.ReadKey();
+        }
+    }
+}
